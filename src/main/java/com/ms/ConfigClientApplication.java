0502e305ad88911664ca0820dc5ba59dea84cd75
askPavan <pavankumarambraji@gmai.com>
2022-07-27 16:57:36 +0530

    added auto refresh

--- src/main/java/com/ms/ConfigClientApplication.java
+++ src/main/java/com/ms/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 
 import com.ms.beans.Student;
@@ -10,12 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-	ApplicationContext context=	SpringApplication.run(ConfigClientApplication.class, args);
-	Student student = context.getBean(Student.class);
-	log.info("Studet Data from Config Server: {}", student);
+		ApplicationContext context = SpringApplication.run(ConfigClientApplication.class, args);
+		Student student = context.getBean(Student.class);
+		log.info("Studet Data from Config Server: {}", student);
 	}
 
 }
